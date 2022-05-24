a75ae15aa9b8f76c0e4d96c7009b134408468452
h0ney98 <87646271+h0ney98@users.noreply.github.com>
2022-05-24 16:37:12 +0300

    Update JPEGER.py

--- tkinter/JPEGER.py
+++ tkinter/JPEGER.py
@@ -1,17 +1,18 @@
-import tkinter as tk  # as переименовывет ткинтер в ТК
+import tkinter as tk  # as переименовывет ткинтер в ТК   
 from tkinter import *
 import webbrowser
 
-JPEGER = tk.Tk()
+JPEGER = tk.Tk()                         
 JPEGER.title(' JPEGER')
 JPEGER.configure(background='#555')
 JPEGER.geometry('600x700+200+100')
 
 
 
-def search():
+def search():           #комманда кнопки
     webbrowser.open('https://vk.com/away.php?to=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FSpecial%3ARandom%2FFile&el=snippet')
 
+#кнопка 
 
 btn1 = Button(text="TOUCH ME ", background="#555", foreground="#ccc",command=search,padx="15", pady="6", font="15")
 btn1.pack(side=BOTTOM)
